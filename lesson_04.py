1ef2e41f7aeb2e1b9ff907bbac6b2d3b144903c0
Pablo Nevares <pnevares@godaddy.com>
2018-08-10 16:52:27 -0700

    updated boolean examples

--- lesson_04.py
+++ lesson_04.py
@@ -1,6 +1,20 @@
-notset = "hello"
+intZero = 0
 
-if notset == None:
-  print("notset is not set")
+if intZero:
+  print("0 is truthy")
 else:
-  print("notset is set")
+  print("0 is not truthy")
+
+stringZero = "0"
+
+if stringZero:
+  print("'0' is truthy")
+else:
+  print("'0' is not truthy")
+
+emptyString = ""
+
+if emptyString:
+  print("'' is truthy")
+else:
+  print("'' is not truthy")
