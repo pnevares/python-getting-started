606feaf86bef332a19fb1b95c35846949add7156
Pablo Nevares <pablo@nevares.com>
2018-08-09 23:40:57 -0700

    more string methods

--- lesson_03.py
+++ lesson_03.py
@@ -5,3 +5,9 @@ print("hello".replace("e", "a") == "hallo")
 print("hello".isalpha() == True)
 print("123".isdigit() == True)
 print("some,more,values".split(",") == ["some", "more", "values"])
+
+print("Hello {0}, welcome to {1}".format("Dave", "space"))
+
+where = "down"
+what = "reverse"
+print(f"I put my thing {where}, flip it, and {what} it")
