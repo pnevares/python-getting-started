879446299ccc4d141d275b5ab474df69e1d6e833
Pablo Nevares <pnevares@godaddy.com>
2018-08-10 16:55:52 -0700

    add example of reference equality aka 'is'

---
+++ lesson_05.py
@@ -0,0 +1,12 @@
+varOne = "string"
+varTwo = varOne
+
+if varOne is varTwo:
+  print("varOne is varTwo")
+else:
+  print("varOne is not varTwo")
+
+if varTwo is varOne:
+  print("varTwo is varOne")
+else:
+  print("varTwo is not varOne")
