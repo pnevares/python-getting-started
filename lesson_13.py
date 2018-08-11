f032e8049fdb148b34991db580a847d945a37e15
Pablo Nevares <pablo@nevares.com>
2018-08-11 14:02:07 -0700

    composing lambdas

--- lesson_13.py
+++ lesson_13.py
@@ -7,3 +7,7 @@ print(f"Six plus three equals {add(6, 3)}")
 print(f"Six minus three equals {subtract(6, 3)}")
 print(f"Six multiplied by three equals {multiply(6, 3)}")
 print(f"Six divided by three equals {divide(6, 3)}")
+
+adder = lambda x: lambda y: add(x, y)
+addSeven = adder(7)
+print(f"Three plus seven is {addSeven(3)}")
