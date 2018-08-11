d7d7d6ecebf1b5c4eceb0c45164c61afd45cb4d1
Pablo Nevares <pablo@nevares.com>
2018-08-11 13:58:06 -0700

    lambda example

---
+++ lesson_13.py
@@ -0,0 +1,9 @@
+add = lambda a, b: a + b
+subtract = lambda a, b: a - b
+multiply = lambda a, b: a * b
+divide = lambda a, b: a / b
+
+print(f"Six plus three equals {add(6, 3)}")
+print(f"Six minus three equals {subtract(6, 3)}")
+print(f"Six multiplied by three equals {multiply(6, 3)}")
+print(f"Six divided by three equals {divide(6, 3)}")
