b118c029f05fa2ffe8dbb2578de48e14b0d672f9
Pablo Nevares <pablo@nevares.com>
2018-08-10 20:12:20 -0700

    advanced list example

---
+++ lesson_08.py
@@ -0,0 +1,16 @@
+words = ["Alpha", "Bravo", "Charlie", "Delta"]
+print(f"words {words}")
+
+print(f"words[1:] {words[1:]}")
+print(f"words[2:] {words[2:]}")
+
+print(f"words[-1:] {words[-1:]}")
+print(f"words[-2:] {words[-2:]}")
+
+print(f"words[:1] {words[:1]}")
+print(f"words[:2] {words[:2]}")
+
+print(f"words[:-1] {words[:-1]}")
+print(f"words[:-2] {words[:-2]}")
+
+print(f"words[1:-1] {words[:-1]}")
