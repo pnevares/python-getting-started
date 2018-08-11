dc7636348ea3d25f1a8f36d64558d44aef616bf8
Pablo Nevares <pablo@nevares.com>
2018-08-11 08:18:44 -0700

    updated function examples

--- lesson_12.py
+++ lesson_12.py
@@ -1,10 +1,23 @@
-def addOne(input):
+def add_one(input):
   return input + 1
+
 
 def add(first = 0, second = 0):
   return first + second
 
-print(f"Five plus one is {addOne(5)}")
+
+def print_var_args(first, *args):
+  print(first)
+  print(args)
+
+def print_kw_args(first, **args):
+  print(first)
+  print(args)
+
+print(f"Five plus one is {add_one(5)}")
 print(f"Six plus two is {add(6, 2)}")
 print(f"Three plus zero is {add(3)}")
 print(f"Zero plus zero is {add()}")
+
+print_var_args("first arg", "second arg", "third arg", "fourth arg")
+print_kw_args("first arg", second="second arg", third="third arg", fourth="fourth arg")
