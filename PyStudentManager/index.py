8efe18e01a66ab692b2d6550e8429ada701e47a4
Pablo Nevares <pablo@nevares.com>
2018-08-11 12:52:18 -0700

    looping through input until five students are entered

--- PyStudentManager/index.py
+++ PyStudentManager/index.py
@@ -4,7 +4,7 @@ students = []
 def get_students_titlecase():
   students_titlecase = []
   for student in students:
-    students_titlecase = student["name"].title()
+    students_titlecase.append(student["name"].title())
   return students_titlecase
 
 
@@ -18,9 +18,10 @@ def add_student(name, student_id=332):
   students.append(student)
 
 
-student_list = get_students_titlecase()
-print(student_list)
+while len(students) < 5:
+  student_name = input("Enter student name: ")
+  student_id = input("Enter student ID: ")
+  add_student(student_name, student_id)
+  print_students_titlecase()
 
-add_student("Mark")
-student_list = get_students_titlecase()
-print(student_list)
+print("Five students reached, ending")
