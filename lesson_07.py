872935c19d3c7254437a7cb2cb358c758eb8e06d
Pablo Nevares <pablo@nevares.com>
2018-08-10 20:01:39 -0700

    added list examples

---
+++ lesson_07.py
@@ -0,0 +1,17 @@
+colors = ["red", "green", "blue"]
+print(colors[0])
+
+colors.append("yellow")
+print(colors[-1])
+
+if "blue" in colors:
+  print("blue is a color")
+else:
+  print("blue is not a color")
+
+print(f"there are {len(colors)} colors")
+
+print(f"deleting last color {colors[-1]}")
+del colors[-1]
+print(f"there are {len(colors)} colors")
+print(f"last color is now {colors[-1]}")
