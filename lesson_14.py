4cd223b3b1633d26143e891c0a0d9aeb1c2c22f7
Pablo Nevares <pablo@nevares.com>
2018-08-11 19:57:24 -0700

    added class attributes

--- lesson_14.py
+++ lesson_14.py
@@ -1,4 +1,6 @@
 class Animal:
+  planet = "Earth"
+
   def __init__(self, name):
     self.name = name
 
@@ -6,7 +8,7 @@ class Animal:
     print("roar")
 
   def __str__(self):
-    return self.name
+    return f"{self.name} on {self.planet}"
 
 bear = Animal("bear")
 print(bear)
@@ -15,3 +17,5 @@ bear.talk()
 human = Animal("human")
 print(human)
 human.talk()
+
+print(Animal.planet)
